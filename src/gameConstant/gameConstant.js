c281d6f17ad147380956bcb51fd5a6e13de64238
Roee030 <roee.angel93@gmail.com>
2021-01-27 13:17:35 +0200

    complete assignment

--- src/gameConstant/gameConstant.js
+++ src/gameConstant/gameConstant.js
@@ -1,15 +1,15 @@
 import MazeSoundSrc from "../audio/maze.mp3";
 import levelEndAudioSrc from "../audio/level_end.mp3";
 
-export const ROUND_TIME = 6;
-export const ROWS = 3;
-export const COLS = 3;
+export const ROUND_TIME = 60;
+export const ROWS = 10;
+export const COLS = 10;
 export const arrowsKeys = [37, 38, 39, 40];
 export const mazeAudio = new Audio(MazeSoundSrc);
 mazeAudio.loop = true;
 export const levelEndAudio = new Audio(levelEndAudioSrc);
-export const lollipopTimer = 4;
-export const iceCreamTimer = 3;
+export const lollipopTimer = 30;
+export const iceCreamTimer = 15;
 export const initialState = {
   points: 0,
   round: 0,
